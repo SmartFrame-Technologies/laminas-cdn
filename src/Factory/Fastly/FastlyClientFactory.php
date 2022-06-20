03065bb87cdc3c5aab5450848f6063a3818b5c9c
krzysztof.guzek <krzysztof.guzek@smartframe.io>
2022-06-20 13:10:27 +0200

    Add null coalescing operator to avoid possible undefined index

--- src/Factory/Fastly/FastlyClientFactory.php
+++ src/Factory/Fastly/FastlyClientFactory.php
@@ -22,7 +22,7 @@ class FastlyClientFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null): Fastly
     {
-        $config = $container->get('config')['cdn']['fastly'];
+        $config = $container->get('config')['cdn']['fastly'] ?? [];
         if (!isset($config['apiToken']) || $config['apiToken'] === ConfigProvider::API_TOKEN_PLACEHOLDER) {
             throw new FastlyApiTokenNotDefinedException();
         }
